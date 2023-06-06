3943d1945fb36710f1d22ba85f4e6958b66441ea
ER RAGRAGUI HATIM <hatim.er@protonmail.com>
2023-06-06 19:38:33 +0200

    adding _redirects file to public

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
