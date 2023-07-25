d428c5b50b3d214f6fc206e6eec30ead90c1780c
EtiamSani <etiam.sani@laposte.net>
2023-07-25 17:39:48 +0200

    Sigup patient features

--- src/pages/SignUp/SignUpPatient.jsx
+++ src/pages/SignUp/SignUpPatient.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { IoEyeSharp } from 'react-icons/io5';
+import { FaEyeSlash } from 'react-icons/fa';
 
 const SingUpPatient = () => {
   const [firstname, setFirstName] = useState('');
@@ -12,7 +14,66 @@ const SingUpPatient = () => {
   const [zipcode, setPostalCode] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [hidePassword, setHidePassword] = useState(true);
+  const [message, setMessage] = useState('');
+  const [progress, setProgress] = useState('');
   const navigate = useNavigate();
+  const [strengthChecks, setStrengthChecks] = useState({
+    length: 0,
+    hasUpperCase: false,
+    hasLowerCase: false,
+    hasDigit: false,
+    hasSpecialChar: false
+  });
+
+  const handlePassword = (passwordValue) => {
+    const strengthChecks = {
+      length: 0,
+      hasUpperCase: false,
+      hasLowerCase: false,
+      hasDigit: false,
+      hasSpecialChar: false
+    };
+
+    const checks = {
+      length: passwordValue.length >= 8,
+      hasUpperCase: /[A-Z]+/.test(passwordValue),
+      hasLowerCase: /[a-z]+/.test(passwordValue),
+      hasDigit: /[0-9]+/.test(passwordValue),
+      hasSpecialChar: /[^A-Za-z0-9]+/.test(passwordValue)
+    };
+
+    setStrengthChecks(checks);
+
+    strengthChecks.length = passwordValue.length >= 8 ? true : false;
+    strengthChecks.hasUpperCase = /[A-Z]+/.test(passwordValue);
+    strengthChecks.hasLowerCase = /[a-z]+/.test(passwordValue);
+    strengthChecks.hasDigit = /[0-9]+/.test(passwordValue);
+    strengthChecks.hasSpecialChar = /[^A-Za-z0-9]+/.test(passwordValue);
+
+    let verifiedList = Object.values(strengthChecks).filter((value) => value);
+
+    let strength =
+      verifiedList.length === 5 ? 'Fort' : verifiedList.length >= 2 ? 'Moyen' : 'Faible';
+
+    setPassword(passwordValue);
+    setProgress(`${(verifiedList.length / 5) * 100}%`);
+    setMessage(strength);
+
+    console.log('verifiedList: ', `${(verifiedList.length / 5) * 100}%`);
+  };
+
+  const getActiveColor = (type) => {
+    if (type === 'Fort') return 'text-green-500 text-xs';
+    if (type === 'Moyen') return 'text-yellow-500 text-xs';
+    return 'text-red-500 text-xs';
+  };
+
+  const getActiveColorBar = (type) => {
+    if (type === 'Fort') return 'bg-green-500';
+    if (type === 'Moyen') return 'bg-yellow-500';
+    return 'bg-red-500';
+  };
 
   const quizz_id = localStorage.getItem('quizId');
 
@@ -93,31 +154,82 @@ const SingUpPatient = () => {
               Mot de passe
             </label>
             <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-4"
-              />
+              <div className="flex">
+                <input
+                  id="password"
+                  name="password"
+                  type={hidePassword ? 'password' : 'text'}
+                  required
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    handlePassword(e.target.value);
+                  }}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-4"
+                />
+                <a onClick={() => setHidePassword(!hidePassword)} href className="-ml-8 mt-2">
+                  {hidePassword ? <IoEyeSharp /> : <FaEyeSlash />}
+                </a>
+              </div>
+              <div
+                className={`progress ${getActiveColorBar(message)}`}
+                style={{
+                  width: progress
+                }}></div>
+            </div>
+
+            <div>
+              {password.length !== 0 ? (
+                <p className={`message ${getActiveColor(message)}`}>
+                  Votre mot de passe est {message}
+                </p>
+              ) : null}
+              {strengthChecks.length &&
+              strengthChecks.hasUpperCase &&
+              strengthChecks.hasLowerCase &&
+              strengthChecks.hasDigit &&
+              strengthChecks.hasSpecialChar ? null : (
+                <>
+                  <div className="card-compact shadow-xl bg-white rounded-xl p-2">
+                    <p className="font-bold mb-2">Le mot de passe doit comporter au moins</p>
+                    {!strengthChecks.length && (
+                      <p className="text-red-400 text-xs">8 caractères.</p>
+                    )}
+                    {!strengthChecks.hasUpperCase && (
+                      <p className="text-red-400 text-xs">Une lettre majuscule.</p>
+                    )}
+                    {!strengthChecks.hasLowerCase && (
+                      <p className="text-red-400 text-xs">Une lettre minuscule.</p>
+                    )}
+                    {!strengthChecks.hasDigit && (
+                      <p className="text-red-400 text-xs">Au moins un chiffre.</p>
+                    )}
+                    {!strengthChecks.hasSpecialChar && (
+                      <p className="text-red-400 text-xs">Au moins un caractère spécial.</p>
+                    )}
+                  </div>
+                </>
+              )}
             </div>
           </div>
+
           <div className="sm:col-span-3">
             <label
               htmlFor="confirmPassword"
               className="block text-sm font-medium leading-6 text-gray-900">
               Confirmer le Mot de passe
             </label>
-            <div className="mt-2">
+            <div className="mt-2 flex">
               <input
                 id="confirmPassword"
                 name="confirmPassword"
-                type="password"
+                type={hidePassword ? 'password' : 'text'}
                 required
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-4"
               />
+              <a onClick={() => setHidePassword(!hidePassword)} href className="-ml-8 mt-2">
+                {hidePassword ? <IoEyeSharp /> : <FaEyeSlash />}
+              </a>
             </div>
           </div>
           <div className="sm:col-span-2 sm:col-start-1">
