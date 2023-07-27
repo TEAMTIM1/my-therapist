82c28344cbfd79facb0b2ad5393e3509baeed8ec
EtiamSani <etiam.sani@laposte.net>
2023-07-27 15:54:48 +0200

    carrousel

--- src/pages/Home/ThirdPage.jsx
+++ src/pages/Home/ThirdPage.jsx
@@ -1,54 +1,63 @@
 import logo from '../../assets/image/therapist-woman.jpg';
 import logo2 from '../../assets/image/therapist-men.jpg';
+import React, { useRef } from 'react';
 
 export const ThirdPage = () => {
+  const carouselRef = useRef(null);
+  const handleArrowClick = (direction) => {
+    if (carouselRef.current) {
+      const carousel = carouselRef.current;
+      const currentScroll = carousel.scrollLeft;
+      const slideWidth = carousel.clientWidth;
+      const targetScroll = direction === 'prev' ? currentScroll - slideWidth : currentScroll + slideWidth;
+      carousel.scrollTo({ left: targetScroll, behavior: 'smooth' });
+    }
+  };
   return (
     <div
       id="carousel-section"
       className="carousel-section flex items-center flex-col px-6 h-screen bg-[#DBCAF4]">
       <div>
-        <h1 className=" mb-4 text-2xl font-semibold">Ceci est un Titre</h1>
+        <h1 className=" mb-10 text-2xl font-semibold">Lorem ipsum dolor sit amet.</h1>
       </div>
-      <div className="carousel  w-auto h-auto md:w-[60%] pb-4">
-        <div id="slide1" className="carousel-item  relative w-full max-md:flex-wrap">
-          <div className="">
-            <img src={logo} alt="/" className=" w-50 h-1/2 rounded-xl " />
+      <div className="carousel w-auto h-auto md:w-[60%] pb-4" ref={carouselRef}>
+        <div id="slide1" className="carousel-item relative w-full max-md:flex-wrap lg:min-w-[70%] flex">
+          <div className="lg:ml-20">
+            <img src={logo} alt="/" className=" w-50 h-1/2 rounded-xl ml-20" />
           </div>
-          <div className=" text-center m-auto">
-            <h3 className="">Un Titre</h3>
+          <div className=" lg:my-20 lg:mr-28  " >
+            <h3 className="font-bold text-xl">Un Titre</h3>
             <span className="text-sm">
-              Ici une belle description de l'image ou d'autres infos sur cette photo Ici une belle
-              description de l'image ou d'autres infos sur cette photo
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, suscipit neque deserunt aliquid maiores ratione.
             </span>
           </div>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-ghost animate-bounce">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-ghost animate-bounce">
-              ❯
-            </a>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-28 lg:top-1/2">
+          <button onClick={() => handleArrowClick('prev')} className="btn btn-ghost animate-bounce">
+          ❮
+        </button>
+        <button onClick={() => handleArrowClick('next')} className="btn btn-ghost animate-bounce">
+          ❯
+        </button>
           </div>
         </div>
 
-        <div id="slide2" className="carousel-item  relative w-full max-md:flex-wrap">
-          <div className="">
-            <img src={logo2} alt="/" className="w-50 h-1/2  rounded-xl" />
+        <div id="slide2" className="carousel-item relative w-full max-md:flex-wrap lg:min-w-[70%] flex space-x-0 ">
+          <div className="lg:ml-20">
+            <img src={logo2} alt="/" className="w-50 h-1/2 rounded-xl ml-20" />
           </div>
-          <div className=" text-center m-auto">
-            <h3 className="">Un Titre</h3>
+          <div className="lg:my-20 lg:mr-28 ">
+            <h3 className="font-bold text-xl">Un Titre</h3>
             <span className="text-sm ">
-              Ici une belle description de l'image ou d'autres infos sur cette photo Ici une belle
-              description de l'image ou d'autres infos sur cette photo
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, suscipit neque deserunt aliquid maiores ratione.
             </span>
           </div>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-ghost animate-bounce">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-ghost animate-bounce">
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-28 lg:top-1/2">
+            <button onClick={() => handleArrowClick('prev')} className="btn btn-ghost animate-bounce ">
+             ❮
+            </button>
+            <button onClick={() => handleArrowClick('next')} className="btn btn-ghost animate-bounce">
               ❯
-            </a>
+            </button>
           </div>
         </div>
       </div>
