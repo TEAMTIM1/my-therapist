15bc2305ab7e073447c8a566c82d3b481397c61b
EtiamSani <etiam.sani@laposte.net>
2023-07-27 16:30:03 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,13 @@ import {AiFillTwitterCircle, AiOutlineInstagram, AiFillLinkedin} from 'react-ico
 
 const Footer = () => {
   return (
-    <div className=' px-4 m-6 divide-y bg-[#DBCAF4]' >
+    <div className=' px-4  divide-y bg-[#A16EE7]' >
         <div className=' grid p-4  pb-4 sm:grid-flow-col'>
           <div className=' grid grid-flow-row gap-4 pt-4 md:justify-center'>
-            <div className=' md:text-xl font-bold'>
+            <div className=' md:text-xl font-bold text-white'>
               <h3>Vous etes</h3>
             </div>
-            <div className=' grid grid-flow-row gap-4'>
+            <div className=' grid grid-flow-row gap-4 text-white'>
               <a href="/patient" >Patient</a>
               <a href="/psycologue" >Psycologue</a>
               <a href="/patient" >Etudiant</a>
@@ -19,20 +19,20 @@ const Footer = () => {
           </div>
 
           <div className='grid grid-flow-row gap-4 pt-4 md:justify-center'>
-            <div className=' md:text-xl font-bold'>
+            <div className=' md:text-xl font-bold text-white'>
               <h3>Devenir notre partenaire</h3>
             </div>
-            <div className=' grid grid-flow-row gap-4'>
+            <div className=' grid grid-flow-row gap-4 text-white'>
               <a href="/patient" >Entreprise</a>
               <a href="/psycologue" >Association</a>
               <a href="/patient" >Ecole</a>
             </div>
           </div>
           <div className=' grid grid-flow-row gap-4 pt-4 md:justify-center'>
-            <div className=' md:text-xl font-bold'>
+            <div className=' md:text-xl font-bold text-white'>
               <h3>À propos</h3>
             </div>
-            <div className=' grid grid-flow-row gap-4 '>
+            <div className=' grid grid-flow-row gap-4 text-white '>
               <a href="/patient" >Comment ca marche ?</a>
               <a href="/psycologue" >Blog</a>
               <a href="/contact">Contactez-nous</a>
@@ -42,10 +42,10 @@ const Footer = () => {
 
         <div className=' grid p-4  pb-4 sm:grid-flow-col '>
           <div className=' grid grid-flow-row gap-4 pt-4 md:justify-center'>
-            <div className=' md:text-xl font-bold'>
+            <div className=' md:text-xl font-bold text-white'>
               <h3>Les Pathologies </h3>
             </div>
-            <div className=' grid grid-flow-row sm:grid-rows-3 sm:grid-flow-col gap-4'>
+            <div className=' grid grid-flow-row sm:grid-rows-3 sm:grid-flow-col gap-4 text-white'>
               <a href="/patient" >Couple</a>
               <a href="/psycologue" >Dépression</a>
               <a href="/patient" >Addiction</a>
@@ -53,11 +53,11 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className='grid grid-flow-row gap-4 pt-4 md:justify-center'>
+          <div className='grid grid-flow-row gap-4 pt-4 md:justify-center text-white'>
             <div className=' md:text-xl font-bold'>
               <h3>Contacter My therapist</h3>
             </div>
-            <div className=' grid grid-flow-row gap-4 text-xs md:text-lg'>
+            <div className=' grid grid-flow-row gap-4 text-xs md:text-lg text-white'>
               <a href="/patient" className=' flex items-center gap-2' ><GiSmartphone/>01454545465</a>
               <a href="/psycologue" className=' flex items-center gap-2'> <MdAttachEmail/> contact@mytherapist.com</a>
               <a href="/patient" className=' grid-flow-col grid' >
@@ -70,10 +70,10 @@ const Footer = () => {
           </div>
 
           <div className=' grid grid-flow-row gap-4 pt-4 md:justify-center'>
-            <div className=' md:text-xl font-bold'>
+            <div className=' md:text-xl font-bold text-white'>
               <h3>Les Thérapies</h3>
             </div>
-            <div className=' text-xs md:text-lg grid grid-flow-row sm:grid-rows-3 sm:grid-flow-col gap-4'>
+            <div className=' text-xs md:text-lg grid grid-flow-row sm:grid-rows-3 sm:grid-flow-col gap-4 text-white'>
               <a href="/patient" >Cognitivo-comportomentale</a>
               <a href="/psycologue" >Psychanalyse</a>
               <a href="/patient" >Psychologie clinique</a>
@@ -81,7 +81,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className=' grid text-xs md:text-lg md:justify-center gap-2 pt-2'>
+        <div className=' grid text-xs md:text-lg md:justify-center gap-2 pt-2 text-white'>
           <div className=' grid md:grid-flow-col md:gap-2'>
             <a href="Condition">Condition Génerale d'utilisation</a>
             -
